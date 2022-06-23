50089422931679b13148b3bc52a5a34d3cfe9a57
Michael "Cal" Krug <cal.krug@protonmail.com>
2022-06-23 13:41:39 -0500

    Add: Finished tutorial

---
+++ src/main/java/com/tts/SpringValidatingFormInput/SpringValidatingFormInputApplication.java
@@ -0,0 +1,13 @@
+package com.tts.SpringValidatingFormInput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidatingFormInputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringValidatingFormInputApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/SpringValidatingFormInput/SpringValidatingFormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.SpringValidatingFormInput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidatingFormInputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
